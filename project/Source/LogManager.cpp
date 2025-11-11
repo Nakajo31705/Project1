0a05f42825685912b3fae32cee97d4636c40b921
nakajo <>
2025-11-12 00:12:14 +0900

    ログマネージャーを少しやった

---
+++ project/Source/LogManager.cpp
@@ -0,0 +1,17 @@
+#include "LogManager.h"
+
+void LogManager::AddMessage()
+{
+}
+
+void LogManager::Update()
+{
+}
+
+void LogManager::Draw()
+{
+}
+
+void LogManager::NextMessage()
+{
+}
--- project/Source/LogManager.h
+++ project/Source/LogManager.h
@@ -1,6 +1,11 @@
 #pragma once
+#include <string>
 
 class LogManager
 {
-
+	LogManager();
+	void AddMessage(std::string& text);
+	void Update();
+	void Draw();
+	void NextMessage();
 };
